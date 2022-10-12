c0b3d93c98aef7be6f3258d6c8eea500313524f3
roslab <yingguang.xing@alumni.fh-aachen.de>
2022-10-12 15:43:11 +0200

    test angle in c020

--- impedance_1/control_1.py
+++ impedance_1/control_1.py
@@ -1,23 +1,19 @@
-# Fachhochschule Aachen
-# Name:Bruce_Xing
-# Time: 2022/10/9 19:43
 import os
 import pybullet as p
 import pybullet_data as pd
 import math
 import time
-from pybullet.impedance_1 import functions
+import functions
 
-
+PI =3.1415926 
 
 p.connect(p.GUI)
 p.setGravity(0, 0, -9.8)
-cheetah3_ID =  p.loadURDF("../pybullet/urdf_model/cheetah3.urdf",[0,0,0.5])
 
+cheetah3_ID =  p.loadURDF("/home/roslab/Remote/1Linux/urdf_model/cheetah3.urdf",[0,0,0.5])
 p.setAdditionalSearchPath(pd.getDataPath())
 planeID = p.loadURDF("plane.urdf")
 # objectUid = p.loadURDF("random_urdfs/000/000.urdf", basePosition=[0.7, 0, 0.7])
-
 
 
 
@@ -26,8 +22,20 @@ count =0
 while True:
     p.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING)
     time.sleep(0.05)
-    count+=1
-    functions.positions_control()
 
 
+    if count >=100:
+        functions.positions_control(cheetah3_ID,[0,PI/4,0],[0,PI/4,0],[0,PI/4,0],[0,PI/4,0])
+
+    
     p.stepSimulation()
+    pos_1=p.getJointStates(cheetah3_ID,[1,4,7,10])[0][0]
+    pos_2=p.getJointStates(cheetah3_ID,[1,4,7,10])[1][0]
+    pos_3=p.getJointStates(cheetah3_ID,[1,4,7,10])[2][0]
+    pos_4=p.getJointStates(cheetah3_ID,[1,4,7,10])[3][0]
+
+
+    print("pos1 = {:.3}, posi_2=={:.3}, pos3=={:.3}, pos4=={:.3}".format(pos_1,pos_2,pos_3,pos_4))
+
+
+    count+=1
